23d27a863511e28db5014c8517f53749f9af7e27
DmNaAl <nda0906@mail.ru>
2026-05-27 22:33:48 +0300

    14

---
+++ Модификатор final и неизменяемость/A.java
@@ -0,0 +1,17 @@
+public class A
+{
+    public final int a;
+	//Можно прямо при объявлении: public final int a = 15;
+	
+	//В конструкторе ЭТОГО класса
+	A(int num)
+	{
+		a = num;
+	}
+	
+	
+	//В таком блоке
+	//{
+	//    a = 1;
+	//}
+}

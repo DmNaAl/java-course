5cdeb6061cf2ef49d10cd2b37055cde82fc20b68
DmNaAl <nda0906@mail.ru>
2026-05-30 21:07:17 +0300

    28

---
+++ Интерфейсы/Interface.java
@@ -0,0 +1,14 @@
+//Спецификаторы protected и private запрещены для такого объявления
+//Работа спецификаторов для интерфейсов полностью совпадает с работой спецификаторов для классов.
+//Однако при вложении в класс интерфейс автоматически становится статическим.
+public interface Interface {
+}
+
+interface Interface2 {
+}
+
+protected interface Interface3 {
+}
+
+private interface Interface4 {
+}
